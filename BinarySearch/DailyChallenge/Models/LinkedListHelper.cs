2a647aba8f824607a3ad440d4b967caa6a5c8e68
Rashmi Singh <34141484+rashmisingh622@users.noreply.github.com>
2023-04-10 19:58:16 +0530

    linked list add feature

---
+++ BinarySearch/DailyChallenge/Models/LinkedListHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DailyChallenge.Models
+{
+	public static class LinkedListHelper
+	{ 
+		public static Node AddNode(this Node startNode, Node newNode)
+		{
+			Node original = startNode;
+			while (startNode.next != null)
+			{
+				startNode = startNode.next;
+			}
+
+			startNode.next = newNode;
+			return original;
+		}
+	}
+}
+
--- BinarySearch/DailyChallenge/Models/Node.cs
+++ BinarySearch/DailyChallenge/Models/Node.cs
@@ -3,8 +3,11 @@ namespace DailyChallenge.Models
 {
 	public class Node
 	{
-		public Node()
+		public int value;
+		public Node next;
+		public Node(int value)
 		{
+			this.value = value;
 		}
 	}
 }
